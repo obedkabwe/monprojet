46ff7e9e83ca808645b6ebc496972e3ac44b5ed6
obedkabwe <kabweobed@gmail.com>
2021-09-23 21:51:28 -0300

    Testando a primeira class de dominio

--- src/main/java/com/obedkabwe/Domain/Categoria.java
+++ src/main/java/com/obedkabwe/Domain/Categoria.java
@@ -1,7 +1,79 @@
 package com.obedkabwe.Domain;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Categoria implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String nome;
+	
+	
+	public Categoria(){
+		
+	}
+
+
+	public Categoria(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return id == other.id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
