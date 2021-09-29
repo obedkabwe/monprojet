c029f5127ef08f2f274c1b979f6c9d5c6d5bbc45
obedkabwe <kabweobed@gmail.com>
2021-09-28 21:05:40 -0300

    criando operacao de instacao

--- src/main/java/com/obedkabwe/ProjpringApplication.java
+++ src/main/java/com/obedkabwe/ProjpringApplication.java
@@ -1,13 +1,49 @@
 package com.obedkabwe;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjpringApplication {
+import com.obedkabwe.Domain.Categoria;
+import com.obedkabwe.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjpringApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1= new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
